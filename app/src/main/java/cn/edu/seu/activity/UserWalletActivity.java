0798a09001c3ea42b15437b8d6717a0c8ae84e8e
yangyang wu <1614468289@qq.com>
2020-03-25 19:10:02 +0800

    创建了wallet和transfer

---
+++ app/src/main/java/cn/edu/seu/activity/UserWalletActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.edu.seu.R;
+
+public class UserWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_wallet);
+}
+}
