4c71ead7c0528479fc685f24b152f1e329fa038c
hupeng <13515616243@163.com>
2020-03-25 18:53:46 +0800

    upload layout

---
+++ app/src/main/java/cn/edu/seu/activity/UploadActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.seu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import cn.edu.seu.R;
+
+import android.os.Bundle;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
