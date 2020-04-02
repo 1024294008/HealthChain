60b95b83161c8d7975eb8c324877d6c0893acde5
yangyang wu <1614468289@qq.com>
2020-04-02 10:52:35 +0800

    alter ther MedicalDetailActivity

--- app/src/main/java/cn/edu/seu/activity/MedicalDetailActivity.java
+++ app/src/main/java/cn/edu/seu/activity/MedicalDetailActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import cn.edu.seu.R;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MedicalDetailActivity extends AppCompatActivity {
+public class MedicalDetailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton back;
+    private ImageView portrait_org;
+    private ImageView portrait_user;
+    private TextView requestService;
+    private TextView organizationName;
+    private TextView serviceName;
+    private TextView introduction;
+    private TextView serviceDetails;
+    private TextView cost;
+    private TextView tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,40 @@ public class MedicalDetailActivity extends AppCompatActivity {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        back = this.findViewById(R.id.back);
+        requestService = this.findViewById(R.id.requestService);
+        portrait_org = this.findViewById(R.id.portrait_org);
+        portrait_user = this.findViewById(R.id.portrait_user);
+        organizationName = this.findViewById(R.id.organizationName);
+        serviceName = this.findViewById(R.id.serviceName);
+        introduction = this.findViewById(R.id.introduction);
+        cost = this.findViewById(R.id.cost);
+        serviceDetails = this.findViewById(R.id.serviceDetails);
+        tel = this.findViewById(R.id.tel);
+
+        back.setOnClickListener(this);
+        requestService.setOnClickListener(this);
+//        portrait_user.setOnClickListener(this);
+//        portrait_org.setOnClickListener(this);
+//        organizationName.setOnClickListener(this);
+//        introduction.setOnClickListener(this);
+//        cost.setOnClickListener(this);
+//        serviceDetails.setOnClickListener(this);
+//        tel.setOnClickListener(this);
+//        serviceName.setOnClickListener(this);
+    }
+
+    //点击事件
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.back:
+                this.finish();
+                break;
+            case R.id.requestService:
+                //弹出对话框
+                break;
         }
     }
 }
