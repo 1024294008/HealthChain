6b9730ea88f61fe7666127849a879542f715b448
hupeng <13515616243@163.com>
2020-04-07 16:44:40 +0800

    http delay

--- app/src/main/java/cn/edu/seu/http/HttpRequest/HttpRequest.java
+++ app/src/main/java/cn/edu/seu/http/HttpRequest/HttpRequest.java
@@ -93,7 +93,7 @@ public class HttpRequest implements HttpApi {
                         t.show();
                     }
                 });
-//        request.setRetryPolicy(new DefaultRetryPolicy(20 * 1000, 1, 1.0f));
+        request.setRetryPolicy(new DefaultRetryPolicy(10 * 1000, 1, 1.0f));
         queue.add(request);
     }
 }
